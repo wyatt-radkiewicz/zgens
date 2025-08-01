7f2e8ec678153d3b6eee322f266993dd829dd865
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-07-31 19:55:12 -0600

    Added more integer helpers

--- src/m68k/int.zig
+++ src/m68k/int.zig
@@ -29,3 +29,86 @@ inline fn extend(To: type, int: anytype) To {
 inline fn as(As: type, int: anytype) As {
     return @truncate(int);
 }
+
+/// Convert an integer to a signedness
+inline fn tosign(
+    to_signedness: std.builtin.Signedness,
+    int: anytype,
+) std.meta.Int(to_signedness, @bitSizeOf(@TypeOf(int))) {
+    return @bitCast(int);
+}
+
+/// Converts a 8 bit int to binary coded decimal
+pub fn tobcd(value: u8) struct { u8, u1 } {
+    const lut = comptime lut: {
+        const lut = [1]u8{0} ** 256;
+        for (0..256) |byte| {
+            const trunc = byte % 100;
+            lut[byte] = (trunc / 10 << 4) | (trunc % 10);
+        }
+        break :lut lut;
+    };
+    return .{ lut[value], @intFromBool(value > 99) };
+}
+
+/// Converts binary coded decimal to 8 bit int
+pub fn frombcd(value: u8) u8 {
+    const lut = comptime lut: {
+        const lut = [1]u8{0} ** 256;
+        for (0..256) |byte| {
+            const ones = byte & 0xf;
+            const tens = byte >> 4;
+            lut[byte] = tens * 10 + ones;
+        }
+        break :lut lut;
+    };
+    return lut[value];
+}
+
+/// Add 'n' integers together
+/// Accepts a tuple of integers
+pub fn add(values: anytype) Op(values) {
+    var op = Op(values){};
+    return inline for (0..values.len) |idx| {
+        if (idx == 0) {
+            op.result = values[idx];
+            continue;
+        }
+        
+        const value = values[idx];
+        op.signed |= @addWithOverflow(tosign(.signed, op.result), tosign(.signed, value))[1];
+        op.unsigned |= @addWithOverflow(tosign(.unsigned, op.result), tosign(.unsigned, value))[1];
+        op.result +%= value;
+    } else op;
+}
+
+/// Substract 'n' integers together
+/// Accepts a tuple of integers
+pub fn sub(values: anytype) Op(values) {
+    var op = Op(values){};
+    return inline for (0..values.len) |idx| {
+        if (idx == 0) {
+            op.result = values[idx];
+            continue;
+        }
+        
+        const value = values[idx];
+        op.signed |= @subWithOverflow(tosign(.signed, op.result), tosign(.signed, value))[1];
+        op.unsigned |= @subWithOverflow(tosign(.unsigned, op.result), tosign(.unsigned, value))[1];
+        op.result -%= value;
+    } else op;
+}
+
+/// The result of a mathmatical operation between 'n' integers
+pub fn Op(values: anytype) type {
+    return struct {
+        /// The value (with overflows)
+        result: if (values.len >= 2) @TypeOf(values[0] +% values[1]) else @TypeOf(values[0]) = 0,
+
+        /// Whether or not signed overflow occurred
+        signed: u1 = 0,
+
+        /// Whether or not unsigned overflow occurred
+        unsigned: u1 = 0,
+    };
+}
